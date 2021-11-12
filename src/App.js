459b93883e1834c89bd851d5d0d0daa2d8c4c390
ayush <kshitij3160@gmail.com>
2021-11-12 05:49:04 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'
+import "../src/App.css"
+import React, {useState,useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+    const [users,setUsers] = useState([]);
+
+    const getData = () => {
+      fetch("https://api.github.com/users").then((response) => {
+        return response.json();
+      }).then((data)=>{
+         setUsers(data);
+        })
+      
 }
+
+    useEffect(() => {
+      getData();
+    },[])
+
+    return (<>
+    <div className="bg-secondary">
+      <div className="container-fluid text-center text-danger">
+        <h1>Github Users</h1>
+    </div>
+    <br/>
+    <br/>
+    <div className = "row">
+    {
+      users.map((value) => {
+        return(<>
+        <div className = "col-sm-4">
+        <div class="card width">
+          <img src={value.avatar_url} className="card-img-top img-he-wi" alt="..." />
+    <div className="card-body">
+    <h5 className="card-title">{value.login}</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href={value.url} className="btn btn-primary">Visit Profile</a>
+    </div>
+    </div>
+    <br/>
+    </div>
+    
+    </>
+        )
+      })
+    }
+    </div>
+    </div>
+      </>
+    )
+  }
+
 
 export default App;
